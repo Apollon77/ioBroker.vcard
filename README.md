008084d64ae35d8cfddf588bfb00ce3bcff3bd42
hometm <tm-mail@gmx.de>
2015-10-02 10:22:11 +0200

    Update V 0.0.2
    refer README.md

--- README.md
+++ README.md
@@ -1,28 +1,40 @@
-vCard Adapter for ioBroker
+#vCard Adapter for ioBroker
+
+##Description:
 
 The main focus of vCard adapter is, to replace the phone numbers of ioBroker.fritzBox adapter's outputs.
 
 This adapter reads a vCard file and offers the possibility to:
 
-a) output of names (full names) on Outputs.FilteredFullNames. 
-b) output of email addresses on Outputs.FilteredEmailAddresses. 
-c) output of postal addresses on Outputs.FilteredPostalAddresses.
+a) output of names (full names) on Outputs.FilteredFullNames.  
+b) output of email addresses on Outputs.FilteredEmailAddresses.  
+c) output of postal addresses on Outputs.FilteredPostalAddresses.  
 d) output of email addresses on Outputs.FilteredEmailAddresses.
+e) the output of a)b)c) and d) can be triggered by setting a search pattern (which is performed on full name)to 
+Inputs.Filter. This filter works case in-sensitive.  
+f) if a value is set to Inputs.ReplacePhoneNumbers, all phone numbers inside this value are replaced by the 
+appropriate full name. The output is written to Outputs.ReplacedPhoneNumbers. Thr replacement ignores the starting 
+'0' and '+49' in German phone numbers.  
+g) Outputs.TodaysBirthdays shows the full name of any persons, which has birthday today.  
+h) if there are multiple results at an output, the different results are separated by an HTML line break.  
 
-e) the output of a)b)c) and d) can be triggered by setting a search pattern (which is performed on full name)to Inputs.Filter.
- This filter works case in-sensitive.
- 
-f) if a value is set to Inputs.ReplacePhoneNumbers, all phone numbers inside this value are replaced by the appropriate full name. 
-The output is written to Outputs.ReplacedPhoneNumbers. Thr replacement ignores the starting '0' and '+49' in German phone numbers.
+_Up to now, the adapter was tested with "Mac contacts" (vCard file version 3.0)._  
 
-g) Outputs.TodaysBirthdays shows the full name of any persons, which has birthday today.
+##Prerequirements:
+- [ioBroker](http://www.ioBroker.net "ioBroker homepage")
 
-h) if there are multiple results at an output, the different results are separated by an "<br>".
-
-
-Up to now, the adapter was tested with "Mac contacts" (vCard file version 3.0).
-
-Prerequirements:
-- ioBroker
 - vcard-json (version >= 0.4.0), in older versions, there is an error regarding email addresses
 - node-schedule (version >= 0.2.9
+
+##Change log:
+
+0.0.2 (2015-10-02)  
+* Documentation updates
+* Missing icon
+* vcard-json issue with white spaces (inside Outlook files)
+
+0.0.1 (2015-09-18)  
+* Initial version
+
+##LOP:  
+* Open file via http
--- vcard.js
+++ vcard.js
@@ -1,42 +1,38 @@
 /**
  *
- * example adapter
+ * vcard adapter
  *
  *
  *  file io-package.json comments:
  *
  *  {
  *      "common": {
- *          "name":         "example",                  // name has to be set and has to be equal to adapters folder name and main file name excluding extension
- *          "version":      "0.0.0",                    // use "Semantic Versioning"! see http://semver.org/
- *          "title":        "Node.js Example Adapter",  // Adapter title shown in User Interfaces
+ *          "name":         "vcard",                  // name has to be set and has to be equal to adapters folder name and main file name excluding extension
+ *          "version":      "0.0.2",                    // use "Semantic Versioning"! see http://semver.org/
+ *          "title":        "vcard Adapter",  // Adapter title shown in User Interfaces
  *          "authors":  [                               // Array of authord
- *              "name <mail@example.com>"
+ *              "name <hometm@gmx.de>"
  *          ]
- *          "desc":         "Example adapter",          // Adapter description shown in User Interfaces. Can be a language object {de:"...",ru:"..."} or a string
+ *          "desc":         "vcard adapter",          // Adapter description shown in User Interfaces. Can be a language object {de:"...",ru:"..."} or a string
  *          "platform":     "Javascript/Node.js",       // possible values "javascript", "javascript/Node.js" - more coming
  *          "mode":         "daemon",                   // possible values "daemon", "schedule", "subscribe"
- *          "schedule":     "0 0 * * *"                 // cron-style schedule. Only needed if mode=schedule
  *          "loglevel":     "info"                      // Adapters Log Level
  *      },
  *      "native": {                                     // the native object is available via adapter.config in your adapters code - use it for configuration
- *          "test1": true,
- *          "test2": 42
  *      }
  *  }
  *
  */
 
 /*
-  Install:
+  Requirements:
      C:\Program Files\ioBroker\node_modules\iobroker.vcard\lib>npm install vcard-json
      C:\Program Files\ioBroker\node_modules\iobroker.vcard\lib>npm install node-schedule
 
 
  */
 
-/* jshint -W097 */// jshint strict:false
-/*jslint node: true */
+
 "use strict";
 
 // you have to require the utils module and call adapter function
@@ -87,50 +83,76 @@ adapter.on('ready', function () {
 });
 
 function main() {
-
+    //adapter.log.info('DEBUG VERSION');
     // in this example all states changes inside the adapters namespace are subscribed
     adapter.subscribeStates('*');
 
 
     // Einlesen der vcard-Datei
-    if(adapter.config.vcardPath=="") {
+    if (adapter.config.vcardPath == "") {
         adapter.log.error('No vcard path configured');
         return;
     }
 
-    vcard.parseFile(adapter.config.vcardPath, function(err, data){
-        if(err) {
-            adapter.log.error('Error reading vcards: '+ err);
-        }
-        else {
-            adapter.log.info('Found '+data.length+' vcards in file');
-            vcards=data;
-            //   var contact;vcards[contact]
-            for(var contact=0;contact< vcards.length;contact++ ){
-                for(var i=0;i< vcards[contact].phone.length;i++){
-                    if(vcards[contact].phone[i].value.indexOf('0049')==0){
-                        //wenn Nummer mit 0049 beginnt, �ndern in +49
-                        if(vcards[contact].phone[i].value.length>4)
-                            vcards[contact].phone[i].value='+49'+vcards[contact].phone[i].value.substring(4,vcards[contact].phone[i].value.length);
-                    }
-                    else if(vcards[contact].phone[i].value.indexOf('00')==0){
-                        //wenn Nummer mit Landesvorwahl (0049,0031...) beginnt
-                        vcards[contact].phone[i].value='+'+vcards[contact].phone[i].value.substring(2,vcards[contact].phone[i].value.length);
-                    }
-                    else if(vcards[contact].phone[i].value.indexOf('0')==0){
-                        //wenn Nummer mit 0 beginnt (z.B. 0721), �ndern in +49721. Die deutsche Landesvorwahl wird hier als default genommen
-                        if(vcards[contact].phone[i].value.length>1)
-                            vcards[contact].phone[i].value='+49'+vcards[contact].phone[i].value.substring(1,vcards[contact].phone[i].value.length);
+    /*
+     //pruefen ob Zugriff via http
+     if(adapter.config.vcardPath.toLowerCase().indexOf('http://')>-1){
+     var xmlHttp=new XMLHttpRequest();
+     xmlHttp.open('GET',adapter.config.vcardPath, false );
+     xmlHttp.send(null);
+
+     if(!xmlHttp.responseText) {
+     adapter.log.error('Error reading vcards via HTTP: '+ adapter.config.vcardPath);
+     adapter.log.error(xmlHttp.statusText);
+     return;
+     }
+     vcards=vcard.parseVcard(xmlHttp.responseText);
+     }*/
+
+    try {
+        vcard.parseFile(adapter.config.vcardPath, function (err, data) {
+            if (err) {
+                adapter.log.error('Error reading vcards: ' + err.message);
+            }
+            else {
+                adapter.log.info('Found ' + data.length + ' vcards in file');
+                vcards = data;
+                try {
+                    for (var contact = 0; contact < vcards.length; contact++) {
+                        for (var i = 0; i < vcards[contact].phone.length; i++) {
+                            if (vcards[contact].phone[i].value.indexOf('0049') == 0) {
+                                //wenn Nummer mit 0049 beginnt, aendern in +49
+                                if (vcards[contact].phone[i].value.length > 4)
+                                    vcards[contact].phone[i].value = '+49' + vcards[contact].phone[i].value.substring(4, vcards[contact].phone[i].value.length);
+                            }
+                            else if (vcards[contact].phone[i].value.indexOf('00') == 0) {
+                                //wenn Nummer mit Landesvorwahl (0049,0031...) beginnt
+                                vcards[contact].phone[i].value = '+' + vcards[contact].phone[i].value.substring(2, vcards[contact].phone[i].value.length);
+                            }
+                            else if (vcards[contact].phone[i].value.indexOf('0') == 0) {
+                                //wenn Nummer mit 0 beginnt (z.B. 0721), aendern in +49721. Die deutsche Landesvorwahl wird hier als default genommen
+                                if (vcards[contact].phone[i].value.length > 1)
+                                    vcards[contact].phone[i].value = '+49' + vcards[contact].phone[i].value.substring(1, vcards[contact].phone[i].value.length);
+                            }
+                        }
                     }
                 }
-            }
-        }
+                catch (e) {
+                    adapter.log.error('Error interpreting file: ' + e.message);
+                    return;
+                }
 
-        getTodayBirthdayPersons();
-    });
-    dailyScheduler = schedule.scheduleJob({hour: 0, minute: 1}, function(){
-        getTodayBirthdayPersons();
+            }
+
+            getTodayBirthdayPersons();
         });
+
+        dailyScheduler = schedule.scheduleJob({hour: 0, minute: 1}, function () {
+            getTodayBirthdayPersons();
+        });
+    } catch (ex) {
+        adapter.log.error('Error reading file: ' + ex.message);
+    }
 }
 
 
@@ -142,7 +164,7 @@ function performNumberString (srcString) {
     var minNumberLength = 4;
     var rtn = "";
 
-    adapter.setState('Inputs.ReplacePhoneNumbers', {ack: true});
+    adapter.setState('Inputs.ReplacePhoneNumbers', {val: srcString, ack: true});
 
     if (!srcString)
         return "";
@@ -244,9 +266,16 @@ function getNameToNumber(number) {
 // Ausgabe der Namen gefilteret
 function getNames(pattern){
     var ret="";
+
+    if(!vcards){
+        adapter.log.info('getNames: vcards not initialized');
+        return;
+    }
+
     for(var contact=0;contact< vcards.length;contact++ ) {
-        if (vcards[contact].fullname.toLowerCase().indexOf(pattern.toLowerCase()) > -1 || pattern == '')
-            ret += vcards[contact].fullname + '<br>';
+        if(vcards[contact].fullname)
+            if (vcards[contact].fullname.toLowerCase().indexOf(pattern.toLowerCase()) > -1 || pattern == '')
+                ret += vcards[contact].fullname + '<br>';
     }
 
     if(ret.length>0){
@@ -263,11 +292,18 @@ function getNames(pattern){
 // Ausgabe der emailadressen ggf. gefilteret
 function getEmailAddresses(pattern){
     var ret="";
+
+    if(!vcards){
+        adapter.log.info('getEmailAddresses: vcards not initialized');
+        return;
+    }
+
     for(var contact=0;contact< vcards.length;contact++ ){
-        if(vcards[contact].fullname.toLowerCase().indexOf(pattern.toLowerCase())>-1 ||  pattern=="" )
-            for(var eNr=0;eNr<vcards[contact].email.length;eNr++) {
-                ret += vcards[contact].email[eNr].value + '<br>';
-            }
+        if(vcards[contact].fullname)
+            if(vcards[contact].fullname.toLowerCase().indexOf(pattern.toLowerCase())>-1 ||  pattern=="" )
+                for(var eNr=0;eNr<vcards[contact].email.length;eNr++) {
+                    ret += vcards[contact].email[eNr].value + '<br>';
+                }
     }
         if(ret.length>0){
             ret=ret.substring(0,ret.length-'<br>'.length);
@@ -283,17 +319,24 @@ function getEmailAddresses(pattern){
 // Ausgabe der Anschrift
 function getPostalAddress(pattern){
     var ret="";
+
+    if(!vcards){
+        adapter.log.info('getPostalAddress: vcards not initialized');
+        return;
+    }
+
     for(var contact=0;contact< vcards.length;contact++ ){
-        if(vcards[contact].fullname.toLowerCase().indexOf(pattern.toLowerCase())>-1 ||  pattern=="" )
-            for(var aNr=0;aNr<vcards[contact].addr.length;aNr++) {
-                ret +=(vcards[contact].fullname)?vcards[contact].fullname+'<br>':"";
-                ret +=(vcards[contact].addr[aNr].street)?vcards[contact].addr[aNr].street+'<br>':"";
-                ret +=(vcards[contact].addr[aNr].zip)?vcards[contact].addr[aNr].zip:"";
-                ret +=(vcards[contact].addr[aNr].city)?' '+ vcards[contact].addr[aNr].city+'<br>':"";
-                ret +=(vcards[contact].addr[aNr].state)?vcards[contact].addr[aNr].state+'<br>':"";
-                ret +=(vcards[contact].addr[aNr].country)?vcards[contact].addr[aNr].country+'<br>':"";
-                ret+='<br>'
-            }
+        if(vcards[contact].fullname)
+            if(vcards[contact].fullname.toLowerCase().indexOf(pattern.toLowerCase())>-1 ||  pattern=="" )
+                for(var aNr=0;aNr<vcards[contact].addr.length;aNr++) {
+                    ret +=(vcards[contact].fullname)?vcards[contact].fullname+'<br>':"";
+                    ret +=(vcards[contact].addr[aNr].street)?vcards[contact].addr[aNr].street+'<br>':"";
+                    ret +=(vcards[contact].addr[aNr].zip)?vcards[contact].addr[aNr].zip:"";
+                    ret +=(vcards[contact].addr[aNr].city)?' '+ vcards[contact].addr[aNr].city+'<br>':"";
+                    ret +=(vcards[contact].addr[aNr].state)?vcards[contact].addr[aNr].state+'<br>':"";
+                    ret +=(vcards[contact].addr[aNr].country)?vcards[contact].addr[aNr].country+'<br>':"";
+                    ret+='<br>'
+                }
     }
 
     if(ret.length>0){
@@ -314,8 +357,12 @@ function getTodayBirthdayPersons(){
     month=(month<10)?'0'+month:month;
     day=(day<10)?'0'+day:day;
     var datePattern='-'+month+'-'+day;
-
     var ret="";
+
+    if(!vcards){
+        adapter.log.info('getTodayBirthdayPersons: vcards not initialized');
+        return;
+    }
 
     for(var contact=0;contact< vcards.length;contact++ ){
         if(vcards[contact].bday.indexOf(datePattern)==4  )
@@ -329,14 +376,23 @@ function getTodayBirthdayPersons(){
     adapter.setState('Outputs.TodaysBirthdays', ret);
 }
 
+
+
 function getPhoneNumbers(pattern){
     var maxHits=5;
     var ret="";
+
+    if(!vcards){
+        adapter.log.info('getPhoneNumbers: vcards not initialized');
+        return;
+    }
+
     for(var contact=0;contact< vcards.length;contact++ ){
-        if(vcards[contact].fullname.toLowerCase().indexOf(pattern.toLowerCase())>-1 ||  pattern=="" )
-            for(var eNr=0;eNr<vcards[contact].phone.length;eNr++) {
-                ret += vcards[contact].phone[eNr].value + '<br>';
-            }
+        if(vcards[contact].fullname)
+            if(vcards[contact].fullname.toLowerCase().indexOf(pattern.toLowerCase())>-1 ||  pattern=="" )
+                for(var eNr=0;eNr<vcards[contact].phone.length;eNr++) {
+                    ret += vcards[contact].phone[eNr].value + '<br>';
+                }
     }
     if(ret.length>0){
         ret=ret.substring(0,ret.length-'<br>'.length);
@@ -347,5 +403,9 @@ function getPhoneNumbers(pattern){
 
 // Setzt Filter ack
 function ackFilter(){
-    adapter.setState('Inputs.Filter',{ack: true});
+    adapter.getState('Inputs.Filter', function (err, res){
+        if(res) {
+            adapter.setState('Inputs.Filter', {val: res, ack: true});
+        }
+    });
 }
